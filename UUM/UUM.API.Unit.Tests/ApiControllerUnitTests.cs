38a930996298822b22fa639d37812118d540dffc
jbezerra <jhonata.empresa@gmail.com>
2020-06-01 00:27:29 -0300

    UUM-2 dev

---
+++ UUM/UUM.API.Unit.Tests/ApiControllerUnitTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace UUM.API.Unit.Tests
+{
+    public class ApiControllerUnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
